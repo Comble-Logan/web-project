4f5fa9fa9ab587dd88dca0b572f1c80d61e577bd
Comble-Logan <logan.comble@student.junia.com>
2024-04-23 20:05:07 +0200

    Footer & Header PHP

--- php/footer.php
+++ php/footer.php
@@ -1,3 +1,29 @@
 <?php
-    // Futur code HTML/PHP pour footer
+    // Futur code php pour gérer la date du copyright
 ?>
+
+<footer class="foot">
+            <div class="footer-container">
+                <div class="footer-contact">
+                    <h3> Nous contacter : </h3>
+                    <ul>
+                        <li> Email : contact@leseptiemeart.com</li>
+                        <li> Tel : 03 27 12 48 19</li>
+                    </ul>
+                </div>
+
+                <div class="footer-information">
+                    <h3> Informations : </h3>
+                    <ul>
+                        <li><a href="html/Mentions_Legales.html"> Mentions Légales </a></li>
+                        <li><a href="html/CGU.html"> Conditions d'Utilisation du site internet </a></li>
+                        <li><a href="html/CGV.html"> Conditions Générales de Vente </a></li>
+                        <li><a href="html/PPDP.html"> Politique de protection des données personnelles </a></li>
+                    </ul>
+                </div>
+
+                <div class="footer-copyrights">
+                    <p>&copy; 1979 - 2024 Le Septième Art. All rights reserved.</p>
+                </div>
+            </div>
+        </footer>
--- php/header.php
+++ php/header.php
@@ -1,3 +1,17 @@
 <?php
-    // Futur code HTML/PHP pour header
+    // Futur code lié a l'affichage du connexion ou profil dans le nav
 ?>
+
+
+<header id="head">
+    <nav class="nav">
+        <img src="images/logo.png" alt="Logo nav" class="nav-logo">
+        <h1 class="nav-name"> Le Septième Art </h1>
+        <ul>
+            <li class="nav-acceuil-page"> <a href="#">Accueil</a> </li>
+            <li class="nav-contact-page"> <a href="#">Contact</a> </li>
+            <li class="nav-connect-page"> <a href="../web-project/HTML/connexion.html">Connexion / Inscription</a> </li>
+        </ul>
+    </nav>
+
+</header>
