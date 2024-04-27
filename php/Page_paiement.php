39cf2298210498989a8c1a3b286adba321ed96be
Lucas-Monnehay <lucas.monnehay@student.junia.com>
2024-04-28 00:11:12 +0200

    mineur

--- php/Page_paiement.php
+++ php/Page_paiement.php
@@ -2,20 +2,11 @@
 <html>
 <head>
     <link rel="stylesheet" type="text/css" href="../css/Page_paiement.css">
-</head>
-<body class="container">
-    <header class="header">
-        <nav class="nav">
-            <img src="../images/logo.png" alt="Logo nav" class="nav-logo">
-            <h1 class="nav-name"> Le Septième Art </h1>
-            <ul>
-                <li class="nav-acceuil-page"> <a href="#">Accueil</a> </li>
-                <li class="nav-contact-page"> <a href="#">Contact</a> </li>
-                <li class="nav-connect-page"> <a href="#">Connexion / Inscription</a> </li>
-            </ul>
-        </nav>
+    <?php
+        include 'header.php';
+    ?>
 
-    </header> 
+        
     <main>
         
     <div class="container2">
@@ -46,32 +37,12 @@
         </div>
         
     </div>
+
+
     </main>
-    <footer class="footer">
-        <div class="footer-container">
-            <div class="footer-contact">
-                <h3> Nous contacter : </h3>
-                <ul>
-                    <li> Email : contact@leseptiemeart.com</li>
-                    <li> Tel : 03 27 12 48 19</li>
-                </ul>
-                
-            </div>
+    <?php
+        include 'footer.php';
+        ?>
 
-            <div class="footer-information">
-                <h3> Informations : </h3>
-                <ul>
-                    <li><a href="html/Mentions_Legales.html"> Mentions Légales </a></li>
-                    <li><a href="html/CGU.html"> Conditions d'Utilisation du site internet </a></li>
-                    <li><a href="html/CGV.html"> Conditions Générales de Vente </a></li>
-                    <li><a href="html/PPDP.html"> Politique de protection des données personnelles </a></li>
-                </ul>
-            </div>
-
-            <div class="footer-copyrights">
-                <p>© 1979 - 2024 Le Septième Art. All rights reserved.</p>
-            </div>
-        </div>
-    </footer>
 </body>
 </html>
