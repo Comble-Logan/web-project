8790bcde702fce6636382c9204b133599899e6e4
Lucas-Monnehay <lucas.monnehay@student.junia.com>
2024-04-28 00:08:55 +0200

    changements mineure

--- php/Page_Reservation.php
+++ php/Page_Reservation.php
@@ -1,16 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    
-<link rel="stylesheet" type="text/css" href="../css/Page_Reservation.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/Page_Reservation.css">
+</head>
+<body class="container">
+    <header class="header">
+        <nav class="nav">
+            <img src="../images/logo.png" alt="Logo nav" class="nav-logo">
+            <h1 class="nav-name"> Le Septième Art </h1>
+            <ul>
+                <li class="nav-acceuil-page"> <a href="#">Accueil</a> </li>
+                <li class="nav-contact-page"> <a href="#">Contact</a> </li>
+                <li class="nav-connect-page"> <a href="#">Connexion / Inscription</a> </li>
+            </ul>
+        </nav>
 
-<div class="container3">
-<?php
-        include 'header.php';
-    ?>
+    </header> 
     <main>
         <div class="global">
-            
+            <button class="bouton2" type="submit" onclick="window.location.href='retour'"><a href="../php/dune.html">RETOUR</a></button>
         <div class="container2">
             <div class="film-details"></div>
                 <img src="../images/Affiches/Affiche-Avatar.jpg" class="image-film" alt="Description de l'image">
@@ -46,12 +57,30 @@
         </div>
     </div>
     </main>
+    <footer class="footer">
+        <div class="footer-container">
+            <div class="footer-contact">
+                <h3> Nous contacter : </h3>
+                <ul>
+                    <li> Email : contact@leseptiemeart.com</li>
+                    <li> Tel : 03 27 12 48 19</li>
+                </ul>
+            </div>
 
-        <?php
-        include 'footer.php';
-        ?>
+            <div class="footer-information">
+                <h3> Informations : </h3>
+                <ul>
+                    <li><a href="html/Mentions_Legales.html"> Mentions Légales </a></li>
+                    <li><a href="html/CGU.html"> Conditions d'Utilisation du site internet </a></li>
+                    <li><a href="html/CGV.html"> Conditions Générales de Vente </a></li>
+                    <li><a href="html/PPDP.html"> Politique de protection des données personnelles </a></li>
+                </ul>
+            </div>
 
-</div>
- 
+            <div class="footer-copyrights">
+                <p>© 1979 - 2024 Le Septième Art. All rights reserved.</p>
+            </div>
+        </div>
+    </footer>
 </body>
 </html>
