34fb366f5944f87c085ac22f4e131f70a4feb4b8
Comble-Logan <logan.comble@student.junia.com>
2024-04-28 18:39:17 +0200

    Modification submit php

--- php/submit.php
+++ php/submit.php
@@ -10,29 +10,26 @@ if (isset($_POST['ok'])) {
 
     if ($count > 0) {
         echo "Vous avez déjà un compte";}
-        else{
+    else{
         $password = $_POST['password'];
         $email = $_POST['email'];
         $birth_date = $_POST['birth_date'];
         $surname = $_POST['surname'];
         $name = $_POST['name'];
-        $isAdmin = isset($_POST['isAdmin'])? 1 : 0;
 
     // Insert data into database
-    $sql = "INSERT INTO users (user_id, password, email, birth_date, surname, name, isAdmin)
-            VALUES (:user_id, :password, :email, :birth_date, :surname, :name, :isAdmin)";
+    $sql = "INSERT INTO users (password, email, birth_date, surname, name, isAdmin)
+            VALUES (:password, :email, :birth_date, :surname, :name, :isAdmin)";
     $stmt = $conn->prepare($sql);
     $stmt->execute(
         array(
-            ":user_id" => $user_id,
             ":password" => $password,
             ":email" => $email,
             ":birth_date" => $birth_date,
             ":surname" => $surname,
             ":name" => $name,
-            ":isAdmin" => $isAdmin,
-        )
-    );
-    header('Location: connexion.php');}
+            ":isAdmin" => 0,));
+        header('Location: connexion.php');
+    }
 }
 ?>
